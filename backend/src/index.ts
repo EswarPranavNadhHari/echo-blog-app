b25fabf78d0f01f6a00316751d38d37ba845e3e4
EswarPranavNadhHari <eswarpranavnadhhari@gmail.com>
2024-09-10 00:33:51 +0530

    feat(routes): allow unauthorized users to read blogs
    Updated routes to enable access for unauthenticated users to view blog posts. Ensured proper handling and security for public access.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,5 +28,81 @@ app.use("*", async (c, next) => {
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
+app.get('/api/v1/bulk', async c => {
+    const prisma = c.get("prisma");
+    try {
+        const posts = await prisma.post.findMany({
+            where: {
+                published: true
+            },
+            distinct: ['id'],
+            select: {
+                id: true,
+                title: true,
+                // content: true,
+                description: true,
+                words: true,
+                image: true,
+                date: true,
+                published: true,
+                authorId: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        });
+        return c.json({
+            blogs: posts
+        })
+    }
+    catch (e) {
+        console.log(e)
+        c.status(400);
+        return c.json({
+            error: "error fetching blogs"
+        })
+    }
+})
+
+app.get('/api/v1/:id', async c => {
+    const prisma = c.get("prisma");
+    const id = c.req.param("id");
+    try {
+        const blogPost = await prisma.post.findFirst({
+            where: {
+                id,
+                published: true
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                description: true,
+                words: true,
+                image: true,
+                date: true,
+                published: true,
+                authorId: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        })
+        return c.json({
+            blog: blogPost
+        })
+    } catch (e) {
+        console.log(e)
+        c.status(204);
+        return c.json({
+            error: "post is not available"
+        })
+    }
+})
+
 
 export default app
